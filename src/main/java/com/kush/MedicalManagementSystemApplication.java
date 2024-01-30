f5e536600c6ac643004b760e3ade182d61556cb2
iamkushmittal <mittalkush1999@gmail.com>
2024-01-31 02:33:15 +0530

    updated applications file with errors removed setup the project for successful run

--- src/main/java/com/kush/MedicalManagementSystemApplication.java
+++ src/main/java/com/kush/MedicalManagementSystemApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+
+
+@SpringBootApplication
 public class MedicalManagementSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -7,7 +7,7 @@
     <!--- basic page needs
     ================================================== -->
     <meta charset="utf-8">
-    <title>Medicine Management System</title>
+    <title>MY Medicine Management System</title>
     <meta name="description" content="">
     <meta name="author" content="">
 
