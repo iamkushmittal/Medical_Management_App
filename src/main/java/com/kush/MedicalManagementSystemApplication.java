6b5f937cefe1d480aacd29d48d5a586a74c45781
iamkushmittal <mittalkush1999@gmail.com>
2024-02-01 23:30:38 +0530

    changes

--- src/main/java/com/kush/MedicalManagementSystemApplication.java
+++ src/main/java/com/kush/MedicalManagementSystemApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-
-
 @SpringBootApplication
 public class MedicalManagementSystemApplication {
 
